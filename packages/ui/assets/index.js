4ff47d3f1d5bbbb15aca62e5d3c52893f005c255
Hem Bahadur Dangaura <mayachy55@gmail.com>
2026-05-18 10:49:43 +0545

    feat(ui): update mobile app home screenshot in how it works section

--- packages/ui/assets/index.js
+++ packages/ui/assets/index.js
@@ -1,3 +1,4 @@
 export { default as Logo } from './logo.png';
 export { default as HeroIllustration } from './hero-illustration.png';
-export { default as AppScreenshot } from './app-screenshot.png';
+export { default as AppScreenshot } from './app-screenshot.jpg';
+
